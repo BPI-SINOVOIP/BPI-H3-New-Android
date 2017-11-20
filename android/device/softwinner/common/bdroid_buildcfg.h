ed1efb93338e292db4c9a5d86e5148631d278757
Justin <Justin>
2017-11-20 10:54:40 +0800

    Modify default bluetooth name

--- android/device/softwinner/common/bdroid_buildcfg.h
+++ android/device/softwinner/common/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "BPI-M2-Plus"
+#define BTM_DEF_LOCAL_NAME "BPI-M2-Plus-Zero"
 
 // SERVICE_CLASS:0x5A (Bit17 -Networking,Bit19 - Capturing,Bit20 -Object Transfer,Bit22 -Telephony)
 // MAJOR CLASS: COMPUTER
